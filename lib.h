5b31af80178ab2a5923989bf43fd0149398bfb19
Your Name <midinfotn401@gmail.com>
2020-12-11 14:18:08 +0100

    window created

---
+++ lib.h
@@ -0,0 +1,16 @@
+#ifndef _DEMO_H_
+#define _DEMO_H_
+#include <SDL2/SDL.h>
+#include <stdio.h>
+/** Screen Demension */
+typedef struct SDL_Instance
+{
+	SDL_Window *window;
+	SDL_Renderer *rendrer;
+
+} SDL_Instance;
+int init_instance(SDL_Instance *);
+void draw (SDL_Instance instance);
+int events (void);
+
+#endif
